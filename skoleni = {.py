336e3d6490658224b9e89146f1e67a00a6bc26d9
Your Name <you@example.com>
2024-09-25 15:37:55 +0300

    v

--- skoleni = {.py
+++ skoleni = {.py
@@ -1,9 +1,13 @@
-skoleni = {
+aa = {
   "Anna": 5,
   "Rainers": 4,
   "Timijs": 6,
   "Mareks": 9,
   "Mārtiņš": 7, #keys ir vārdi
 }
-x=skoleni.keys() #ja gribi ar cipariem neraksti . keys
+x=aa.keys() #ja gribi ar cipariem neraksti . keys
 print(x)
+y=aa.values()
+print(y)
+z=aa.get('Mārtiņš')
+print(z)
