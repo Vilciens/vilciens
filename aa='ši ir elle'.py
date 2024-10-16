060bfeba1831f6d5467bae04718efebc8a1ef97d
Your Name <you@example.com>
2024-10-16 14:41:03 +0300

    count

---
+++ aa='ši ir elle'.py
@@ -0,0 +1,5 @@
+aa='ši ir elle'
+aa.split()
+print('teikums ir',len(x),'vārdu garš')
+aa.lower()
+print(aa.count('a'))
--- skoleni = {.py
+++ skoleni = {.py
@@ -1,18 +1,31 @@
 aa = {
+<<<<<<< HEAD
   "Audi": 2009,
   "Golfs": 2007,
   "Toyota": 2005,
   "Tesla": 2022,
   "Bembis": 2004,
     #keys ir vārdi
+=======
+  "Anna": 5,
+  "Rainers": 4,
+  "Timijs": 6,
+  "Mareks": 9,
+  "Mārtiņš": 7, #keys ir vārdi
+>>>>>>> 0d9bcf5a19393d50c8bce54adaef7fa377dd4101
 }
 x=aa.keys() #ja gribi ar cipariem neraksti . keys
 print(x)
 y=aa.values()
 print(y)
+<<<<<<< HEAD
 for aa, year in aa.items():
   print(aa,year)
 if'Audi' in aa:
   print('audi nav vardnica')
 else:
   print('audi ir vardnica')
+=======
+z=aa.get('Mārtiņš')
+print(z)
+>>>>>>> 0d9bcf5a19393d50c8bce54adaef7fa377dd4101
