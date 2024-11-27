b001920c891a7d2a27a26f8c4135d387c21ef477
Your Name <you@example.com>
2024-11-27 14:36:22 +0200

    ludzu

--- opencsv.py
+++ opencsv.py
@@ -1,11 +1,19 @@
 import csv
-
-with open('veseliba.csv', mode='r', newline='',encoding='latin-1 ') as file:
-    reader=csv.reader(file)
-    for row in reader:
-        novads=row[0]
-        pagasts=row[1]
-        dzimte=row[2]
-        gadi=row[3]
-
-        novads
+from collections import Counter
+novads_counter=Counter()
+try:
+    with open('veseliba.csv', mode='r', newline='',encoding='utf-8 ') as file:
+        reader=csv.reader(file)
+        header=next(reader)
+        for row in reader:
+            if len(row)>1:
+                novads=row[1].strip()
+                novads_counter[novads] +=1
+    most_common_novads=novads_counter.most_common(1)
+    print('Novads kurš visvairāk atkārtojas:')
+    for novads, count in most_common_novads:
+        print(f'{novads}: {count} reizes')
+except FileNotFoundError:
+    print("Fails 'veseliba.csv' netika atrasts. Pārbaudi faila atrašanās vietu.")
+except Exception as e:
+    print(f"Kļūda: {e}")
